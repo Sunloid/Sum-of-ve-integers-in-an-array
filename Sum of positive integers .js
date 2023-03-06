424add33316ebe629473119c2477f4628c404243
Syed ahmed haider <123230005+Sunloid@users.noreply.github.com>
2023-03-06 23:43:21 +0530

    Add files via upload

---
+++ Sum of positive integers .js
@@ -0,0 +1,15 @@
+//A function that ctakes an array of integers and returns the sum of all the positive integers of the array.
+var positiveSum = (arr1) =>{
+    var arrayLength = arr1.length
+    var sumIntegers = 0
+    for(var i =0;i<arrayLength;i++){
+        var temp = arr1[i]
+        if (temp>0){
+            
+            sumIntegers += arr1[i]
+        }  
+    }
+    return sumIntegers
+}
+var result = positiveSum([-8,8,9,12,-85])
+console.log(result)
